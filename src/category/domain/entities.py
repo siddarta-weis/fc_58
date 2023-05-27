02d986857108dd4d49fea3a14b8ed6c22c4e98b6
Siddarta Weissbock <siddarta.weis@gmail.com>
2023-05-27 16:02:40 -0300

    feat: category da aplicação

---
+++ src/category/domain/entities.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
+import uuid
+
+@dataclass(kw_only=True)
+class Category:
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
