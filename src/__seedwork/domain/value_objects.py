651fb84e4962937f23a9b3380a39c027ba7bc51e
Siddarta Weissbock <siddarta.weis@gmail.com>
2023-05-27 16:05:09 -0300

    feat: adicionado value object

---
+++ src/__seedwork/domain/value_objects.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from __seedwork.domain.exceptions import InvalidUuidException
+import uuid
+
+
+@dataclass()
+class UniqueEntityId:
+    id: str = field(
+        default_factory=lambda: str(uuid.uuid4())
+    )
+
+    def __post_init__(self):
+        self.id = str(self.id) if isinstance(self.id, uuid.UUID) else self.id
+        self.__validate()
+
+    def __validate(self):
+        try:
+            uuid.UUID(self.id)
+        except ValueError as ex:
+            raise InvalidUuidException() from ex
