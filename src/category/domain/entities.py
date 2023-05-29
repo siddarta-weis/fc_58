95a0d19ff683f070139457fc0b988f6031f7cbbe
Siddarta Weissbock <siddarta.weis@gmail.com>
2023-05-29 17:49:01 -0300

    feat: adicionado operação de update

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,12 +1,15 @@
 from datetime import datetime
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, replace
 from typing import Optional
 from __seedwork.domain.entities import UniqueEntityId
 
 
-@dataclass(kw_only=True, frozen=True)
+@dataclass(kw_only=True, frozen=True, slots=True)
 class Category(UniqueEntityId):
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+
+    def update(self, name: str, description: str):
+        return replace(self, name=name, description=description)
